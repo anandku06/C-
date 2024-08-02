56ed46b6e12d209b927ae739b4be738a921feb02
Anand Kumar <161126335+anandku06@users.noreply.github.com>
2024-08-02 14:56:44 +0530

    Completed Pattern 7 and 8

---
+++ Patterns/pattern7.cpp
@@ -0,0 +1,39 @@
+/*
+Write a program to print the following pattern:
+    *    
+   ***   
+  *****
+ *******
+*********
+*/
+
+#include <iostream>
+using namespace std;
+
+void printPattern(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++) // spaces
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i+1; j++) // stars
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++) // spaces
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
