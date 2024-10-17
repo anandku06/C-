866ee8ad44f5c45033aab4e5d4ae44bb7cc15405
Anand Kumar <161126335+anandku06@users.noreply.github.com>
2024-10-17 17:06:26 +0530

    Learned Postorder Traversal

---
+++ DSA/Tree/postOrder.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr){}
+};
+
+
+vector<int> postOrderTraversal(TreeNode* root)
+{
+    vector<int> postorder;
+    if(root == nullptr) return postorder;
+
+    stack<TreeNode*> st1, st2;
+    st1.push(root);
+
+    while (!st1.empty())
+    {
+        root = st1.top();
+        st1.pop();
+        st2.push(root);
+
+        if(root->left != nullptr)
+        st1.push(root->left);
+
+        if(root->right != nullptr)
+        st1.push(root->right);
+    }
+
+    while (!st2.empty())    
+    {
+        postorder.push_back(st2.top() -> data);
+        st2.pop();
+    }
+
+    return postorder;    
+}
