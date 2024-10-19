0bf4867dd37caab80ec1c6b4869c6694c22da4df
Anand Kumar <161126335+anandku06@users.noreply.github.com>
2024-10-19 18:05:12 +0530

    Learned Pre, In and Post Order
    PreOrder, InOrder and PostOrder in a single Traversal

--- DSA/Tree/Binary Tree/preInPost.cpp
+++ DSA/Tree/Binary Tree/preInPost.cpp
@@ -22,6 +22,28 @@ void preInPostTraversal(TreeNode* root)
     {
         auto it = st.top();
         st.pop();
+
+        if(it.second == 1)
+        {
+            pre.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+
+            if(it.first->left != nullptr)
+            st.push({it.first->left, 1});
+        }
+
+        else if(it.second == 2)
+        {
+            in.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+
+            if(it.first->right != nullptr)
+            st.push({it.first->right, 1});
+        }
+        
+        else post.push_back(it.first->data);
     }
     
 }
