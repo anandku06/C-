48c988341d178d364f28f09715bccab8b4b35827
Commiter <161126335+anandku06@users.noreply.github.com>
2024-10-29 18:11:47 +0530

    Update Insertion.cpp
    Insertion at the end of the Singly LL

--- DSA/Linked Lists/Singly/Insertion.cpp
+++ DSA/Linked Lists/Singly/Insertion.cpp
@@ -4,14 +4,34 @@ using namespace std;
 struct Node
 {
     int data;
-    Node* next;
+    Node *next;
 
-    Node(int x) : data(x), next(nullptr){}
+    Node(int x) : data(x), next(nullptr) {}
 };
 
-Node* insertAtTheHead(Node* head, int val)
+Node *insertAtTheHead(Node *head, int val)
 {
-    Node* temp = new Node(val);
+    Node *temp = new Node(val);
     temp->next = head;
     return temp;
+}
+
+Node *insertAtTheEnd(Node **head, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (*head == nullptr)
+    {
+        *head = newNode;
+        return *head;
+    }
+
+    Node *temp = *head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return *head;
 }
