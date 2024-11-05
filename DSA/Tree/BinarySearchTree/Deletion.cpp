ef2b5bcacb55e7b98263d01a4a118d1ddefa8924
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-05 14:25:14 +0530

    Create Deletion.cpp
    Deletion in BST

---
+++ DSA/Tree/BinarySearchTree/Deletion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+};
+
+
+int main()
+{
+    
+    return 0;
+}
