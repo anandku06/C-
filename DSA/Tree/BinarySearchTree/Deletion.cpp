15ba3b082ced7acbc31e7ca45a3465d564768474
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-05 21:52:15 +0530

    Update Deletion.cpp
    Deletion in BST (incomplete)

--- DSA/Tree/BinarySearchTree/Deletion.cpp
+++ DSA/Tree/BinarySearchTree/Deletion.cpp
@@ -4,15 +4,53 @@ using namespace std;
 struct TreeNode
 {
     int data;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 
     TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
+TreeNode *helper(TreeNode *);
+TreeNode *findLastChild(TreeNode *);
+
+TreeNode *deletionInBST(TreeNode *root, int key)
+{
+    if (root == nullptr)
+        return nullptr;
+
+    if (root->data == key)
+        return helper(root);
+
+    TreeNode *temp = root;
+    while (temp != nullptr)
+    {
+        if (key < temp->data)
+        {
+            if (temp->left != nullptr && temp->left->data == key)
+            {
+                temp->left = helper(temp->left);
+                break;
+            }
+            else
+                temp = temp->left;
+        }
+        else
+        {
+            if (temp->right != nullptr && temp->right->data == key)
+            {
+                temp->right = helper(temp->right);
+                break;
+            }
+            else
+                temp = temp->right;
+        }
+    }
+
+    return root;
+}
 
 int main()
 {
-    
+
     return 0;
 }
