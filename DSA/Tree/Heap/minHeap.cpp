10e7a03ae972ecdfc06ca69903616a0559c3d37d
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-13 23:07:49 +0530

    Refining of Code and Insertion in minHeap

--- DSA/Tree/Heap/minHeap.cpp
+++ DSA/Tree/Heap/minHeap.cpp
@@ -7,20 +7,38 @@ void heapify(int arr[], int size, int i)
     int l = 2 * i + 1;
     int r = 2 * i + 2;
 
-    if(l < size && arr[smallest] > arr[l])
-    smallest = l;
+    if (l < size && arr[smallest] > arr[l])
+        smallest = l;
 
-    if(r < size && arr[smallest] > arr[r])
-    smallest = r;
+    if (r < size && arr[smallest] > arr[r])
+        smallest = r;
 
-    if(smallest != i)
+    if (smallest != i)
     {
         swap(arr[i], arr[smallest]);
         heapify(arr, size, smallest);
     }
 }
 
-void insertion(int arr[], int size, int value)
+void insertion(int arr[], int &size, int value)
 {
-    
+    if (size == 0)
+    {
+        arr[0] = value;
+        size++;
+    }
+
+    else
+    {
+        arr[size] = value;
+        size++;
+
+        int i = size - 1;
+
+        while (i > 0 && arr[i] < arr[(i - 1) / 2])
+        {
+            swap(arr[i], arr[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
 }
