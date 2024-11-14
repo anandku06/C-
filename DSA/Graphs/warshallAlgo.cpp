01c2bbd57c99ff0c0367cd69518c23d9ad30ff3f
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-14 11:02:25 +0530

    Learned floydWarshall Algo in graphs

--- DSA/Graphs/warshallAlgo.cpp
+++ DSA/Graphs/warshallAlgo.cpp
@@ -1,8 +1,37 @@
 #include <bits/stdc++.h>
+#define nV 4
+#define INF 999
 using namespace std;
+
+void floydWarshall(int graph[nV][nV])
+{
+    int matrix[nV][nV], i, j, k;
+
+    for (int i = 0; i < nV; i++)
+    {
+        for (int j = 0; j < nV; j++)
+        {
+            matrix[i][j] = graph[i][j];
+        }
+    }
+
+    for (int k = 0; k < nV; k++)
+    {
+        for (int i = 0; i < nV; i++)
+        {
+            for (int j = 0; j < nV; j++)
+            {
+                if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+            }
+        }
+    }
+
+    printMatrix(matrix[nV][nV]);
+}
 
 int main()
 {
-    
+
     return 0;
 }
