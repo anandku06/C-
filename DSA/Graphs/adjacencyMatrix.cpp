b236eec9a5f781309b22a7ab36963f678c61072b
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-21 00:55:15 +0530

    Adajcency Matrix for directed graph

--- DSA/Graphs/adjacencyMatrix.cpp
+++ DSA/Graphs/adjacencyMatrix.cpp
@@ -8,6 +8,7 @@ int main()
 
     int adjacencyMatrix[n + 1][m + 1];
 
+    // undirected graph
     for (int i = 0; i < m; i++)
     {
         int u, v;
@@ -15,6 +16,15 @@ int main()
 
         adjacencyMatrix[u][v] = 1;
         adjacencyMatrix[v][u] = 1;
+    }
+
+    // directed graph
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adjacencyMatrix[u][v] = 1;
     }
     
     return 0;
