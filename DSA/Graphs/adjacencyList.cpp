dd3c2bff0a850d60c95fa5e788516b2d4917a14e
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-21 01:00:53 +0530

    Made Adjacency List for undirected graph

---
+++ DSA/Graphs/adjacencyList.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> adjacencyList[n + 1];
+
+    // undirected graph
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adjacencyList[u].push_back(v);
+        adjacencyList[v].push_back(u);
+    }
+    
+    return 0;
+}
